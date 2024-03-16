1466d45d9879782cc6f19cf140146b87494b7cc8
Divya R <kwrdivyaraj100@gmail.com>
2024-03-16 12:22:19 +0530

    Creating Product component to display individual product details with an Add to cart button

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function Product() {
+import "./Product.css";
+const Product = ({ name, id, imgURL, price }) => {
     return (
-        <div>Product</div>
-    )
-}
+        <div className="card">
+            <img src={imgURL} alt={name} />
+            <h2>{name}</h2>
+            <p>$ {price}</p>
+            <button>Add to cart</button>
+        </div>
+    );
+};
 
 export default Product;
