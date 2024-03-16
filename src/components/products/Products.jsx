97a124ff4120b2f0b81613cc8613a89a2b3ab500
Divya R <kwrdivyaraj100@gmail.com>
2024-03-16 12:32:45 +0530

    Adding Products component with dummy data

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
+import Product from "../product/Product";
 
-function Products() {
+const DUMMY_PRODUCTS = [
+    {
+        id: 1,
+        name: "MacBook",
+        imgURL: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        price: 25,
+    },
+    {
+        id: 2,
+        name: "Lenovo Yoga",
+        imgURL: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        price: 25,
+    },
+    {
+        id: 3,
+        name: "Dell Latitude",
+        imgURL: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        price: 25,
+    },
+];
+
+const Products = () => {
     return (
-        <div>Products</div>
-    )
-}
+        <div>
+            <ul className="products-container">
+                {DUMMY_PRODUCTS.map((product) => (
+                    <li key={product.id}>
+                        <Product
+                            id={product.id}
+                            name={product.name}
+                            imgURL={product.imgURL}
+                            price={product.price}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default Products;
