64d34a449a0026a5998b23aed65ea17d57538022
Divya R <kwrdivyaraj100@gmail.com>
2024-03-16 10:36:06 +0530

    Adding authentication form component

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function Auth() {
+export default function Auth() {
     return (
-        <div>Auth</div>
-    )
+        <div>
+            <div className="container">
+                <h1>Login</h1>{" "}
+                <form>
+                    <label htmlFor="id">Id</label>
+                    <input type="text" name="id" id="id" />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                    <button className="login-btn" type="submit">
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
-
-export default Auth;
