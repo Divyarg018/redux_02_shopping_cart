8e8e67f4228e31b6b5c5f661e08c1010127d8cac
Divya R <kwrdivyaraj100@gmail.com>
2024-03-16 12:34:27 +0530

    Adding additional dummy products to Products component

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -20,6 +20,19 @@ const DUMMY_PRODUCTS = [
         imgURL: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         price: 25,
     },
+
+    {
+        id: 4,
+        name: "HP Pavilion",
+        imgURL: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        price: 25,
+    },
+    {
+        id: 5,
+        name: "Acer Aspire",
+        imgURL: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        price: 25,
+    },
 ];
 
 const Products = () => {
