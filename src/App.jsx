9fa11b5fa4c2db90d53ac617be9c1f684f8db85f
Divya R <kwrdivyaraj100@gmail.com>
2024-03-18 20:09:18 +0530

    Using and creating Thunk pattern with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import './App.css';
 import Auth from './components/auth/Auth.jsx';
 import Layout from './components/layout/Layout.jsx';
 import { useEffect } from "react";
+import Notification from "./components/notification/Notification.jsx";
+import { uiActions } from "./store/ui_slice.js";
+import { sendCartData } from "./store/cart_slice.js";
+
+let isFirstRender = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification)
   const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   // console.log(cartItems);
   useEffect(() => {
-    async function sendRequest() {
-      const res = await fetch(
-        'https://redux-http-14b55-default-rtdb.firebaseio.com/cartItems.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-      const data = await res.json();
-    };
-    sendRequest();
-  }, [cart]);
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
+
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
   return (
     <div >
+      {notification && <Notification type={notification.type} message={notification.message} />}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
--- src/store/cart_slice.js
+++ src/store/cart_slice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui_slice";
+
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
@@ -45,5 +47,42 @@ const cartSlice = createSlice({
         },
     },
 });
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(
+            uiActions.showNotification({
+                open: true,
+                message: "Sending Request",
+                type: "warning",
+            })
+        );
+        async function sendRequest() {
+            const res = await fetch(
+                'https://redux-http-14b55-default-rtdb.firebaseio.com/cartItems.json',
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart),
+                }
+            );
+            const data = await res.json();
+            dispatch(uiActions.showNotification({
+                open: true,
+                message: "Sent Request to Database Successfully",
+                type: 'success'
+            })
+            );
+        };
+        try {
+            await sendRequest();
+        } catch (err) {
+            dispatch(uiActions.showNotification({
+                open: true,
+                message: "Sending Request Failed",
+                type: 'error'
+            }))
+        }
+    }
+}
 export const cartActions = cartSlice.actions;
 export default cartSlice;
