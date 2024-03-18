2482746823ceb25e34e34451f58ed2e49422f8a9
Divya R <kwrdivyaraj100@gmail.com>
2024-03-18 13:02:54 +0530

    Using firebase with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useSelector } from "react-redux";
 import './App.css';
 import Auth from './components/auth/Auth.jsx';
-import Layout from './components/layout/Layout.jsx'
+import Layout from './components/layout/Layout.jsx';
+import { useEffect } from "react";
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemList);
-  console.log(cartItems);
+  const cart = useSelector((state) => state.cart);
+  // console.log(cartItems);
+  useEffect(() => {
+    fetch('https://redux-http-14b55-default-rtdb.firebaseio.com/cartItems.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    })
+  }, [cart])
   return (
     <div >
       {!isLoggedIn && <Auth />}
