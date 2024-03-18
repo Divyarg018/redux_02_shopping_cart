04ce9fc0d977887c41681917abe538f6014783fe
Divya R <kwrdivyaraj100@gmail.com>
2024-03-18 13:09:47 +0530

    Sending asynchronous HTTP requests with redux

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import Layout from './components/layout/Layout.jsx';
 import { useEffect } from "react";
 
 function App() {
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const cart = useSelector((state) => state.cart);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   // console.log(cartItems);
   useEffect(() => {
-    fetch('https://redux-http-14b55-default-rtdb.firebaseio.com/cartItems.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    })
-  }, [cart])
+    const sendRequest = async () => {
+      const res = await fetch(
+        'https://redux-http-14b55-default-rtdb.firebaseio.com/cartItems.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+    };
+    sendRequest();
+  }, [cart]);
   return (
     <div >
       {!isLoggedIn && <Auth />}
