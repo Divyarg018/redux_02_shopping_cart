462d3ecbcd2296d2c098b10c47a6c63ebc0cc6be
Divya R <kwrdivyaraj100@gmail.com>
2024-03-18 11:43:22 +0530

    Rendering cart items in Cart component using map function

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from '../header/Header.jsx';
 import Products from '../products/Products.jsx';
 import "./Layout.css";
+import CartItems from "../cartitems/CartItems.jsx";
+
 const Layout = function () {
     let total = 100;
 
@@ -10,6 +12,7 @@ const Layout = function () {
             <div className="layout">
                 <Header />
                 <Products />
+                <CartItems />
                 <div className="total-price">
                     <h3>Total: ${total}</h3>
                     <button className="orderBtn">Place Order</button>
