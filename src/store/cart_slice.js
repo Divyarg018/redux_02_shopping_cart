f81090828f54d388f4accc01a646fb62f98b2389
Divya R <kwrdivyaraj100@gmail.com>
2024-03-18 20:14:36 +0530

    Adding sendCartData action creator for sending cart data to the server

--- src/store/cart_slice.js
+++ src/store/cart_slice.js
@@ -48,41 +48,6 @@ const cartSlice = createSlice({
     },
 });
 
-export const sendCartData = (cart) => {
-    return async (dispatch) => {
-        dispatch(
-            uiActions.showNotification({
-                open: true,
-                message: "Sending Request",
-                type: "warning",
-            })
-        );
-        async function sendRequest() {
-            const res = await fetch(
-                'https://redux-http-14b55-default-rtdb.firebaseio.com/cartItems.json',
-                {
-                    method: 'PUT',
-                    body: JSON.stringify(cart),
-                }
-            );
-            const data = await res.json();
-            dispatch(uiActions.showNotification({
-                open: true,
-                message: "Sent Request to Database Successfully",
-                type: 'success'
-            })
-            );
-        };
-        try {
-            await sendRequest();
-        } catch (err) {
-            dispatch(uiActions.showNotification({
-                open: true,
-                message: "Sending Request Failed",
-                type: 'error'
-            }))
-        }
-    }
-}
+
 export const cartActions = cartSlice.actions;
 export default cartSlice;
